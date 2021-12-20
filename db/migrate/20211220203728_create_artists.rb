698f961adffd2b151ba344e0a4ae19d366b9f21e
Louise Iyengar <louise.iyengar@flatironschool.com>
2021-12-20 16:16:35 -0500

    commit tree f00c66f4bcf91e94607a6b30875044e68be33493

---
+++ db/migrate/20211220203728_create_artists.rb
@@ -0,0 +1,13 @@
+# db/migrate/20210716095220_create_artists.rb
+
+class CreateArtists < ActiveRecord::Migration[6.1]
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+      # the id column is gemerated automatically for every table! no need to specify it here.
+    end
+  end
+end
